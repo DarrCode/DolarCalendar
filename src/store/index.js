78ce2e18d2c71092dbecd194cf47685c0eda939c
darr2020 <diego.rojas0640@gmail.com>
2020-03-18 19:56:00 -0400

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loading:{
+      title: '',
+      estado: false,
+      color: 'primary'
+    }
   },
   mutations: {
+    showLoad(state, playload){
+      state.loading.title = playload.title;
+      state.loading.estado = true;
+      state.loading.color = playload.color;
+    },
+    hideLoad(state){
+      state.loading.estado = false;
+    }
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row :wrap="true">
+      <v-col md12>
+        <v-card>
+          <v-date-picker v-model="fecha" 
+            full-width
+            locale="es-ve"
+            :min="min"
+            :max="max"
+            @change="getDolar(fecha)">
+          
+          </v-date-picker>
+        </v-card>
+        <v-card color="info" light>
+          <v-card-text class="display-1 text-md-center">
+           {{valor}}
+          </v-card-text>
+           
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios';
+import { mapMutations } from 'vuex';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      fecha: new Date().toISOString().substr(0, 10),
+      min: '2000',
+      max: new Date().toISOString().substr(0, 10),
+      valor: null
+    }
+  },
+  methods: {
+    ...mapMutations(['showLoad', 'hideLoad']),
+    async getDolar(day){
+      let ddmmyy = day.split('-').reverse().join('-');
+      try {
+        this.showLoad({title: 'Cargando datos', color: 'secondary'})
+        let dates = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`);
+
+        if (dates.data.serie.length == 0) {
+          this.valor = "Los fines de semana no se taza el dolar"
+        }else{
+          this.valor = await dates.data.serie[0].valor;
+
+        }
+        
+      } catch(e) {
+        console.log(e);
+      } finally{
+        this.hideLoad();
+      }
+    }
+  },
+  mounted() {
+    this.getDolar(this.fecha);
+  },
 }
 </script>
